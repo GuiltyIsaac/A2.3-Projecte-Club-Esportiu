315b6580e27a2fd48bc307d60809fd8c501f3b2f
GuiltyIsaac <158332708+GuiltyIsaac@users.noreply.github.com>
2024-02-01 16:50:50 +0100

    Update club_esportiu.sql

--- club_esportiu.sql
+++ club_esportiu.sql
@@ -5,7 +5,7 @@ CREATE TABLE socis(
     edat NUMBER(3),
     numCompte VARCHAR2(24)UNIQUE NOT NULL,
     email VARCHAR2(40) UNIQUE,
-    telf NUMBER(9)UNIQUE NOT NULL
+    telf NUMBER(9)UNIQUE NOT NULL,
 
     CONSTRAINT pk_socis PRIMARY KEY(id)
 );
@@ -20,13 +20,20 @@ CREATE TABLE activitats(
     
     CONSTRAINT pk_activitats PRIMARY KEY(id)
 );
-CREATE TABLE Monitors(
+CREATE TABLE monitors(
     dni VARCHAR2(9),
     nom VARCHAR2(50),
     cognoms VARCHAR2(50),
     email VARCHAR2(500) UNIQUE NOT NULL,
     telf VARCHAR2(500) UNIQUE NOT NULL, 
-    compte VARCHAR2(50) UNIQUE NOT NULL
+    compte VARCHAR2(50) UNIQUE NOT NULL,
 
     CONSTRAINT pk_dni_monitor PRIMARY KEY(dni)
+);
+CREATE TABLE seccio(
+    id NUMBER(11),
+    nom VARCHAR2(50),
+    desc. VARCHAR2(500),
+    
+CONSTRAINT pk_id PRIMARY KEY(id)
 );
